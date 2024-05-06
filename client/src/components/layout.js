031ea4cb0d640abaf7b2d26200efa61872fecc01
AsilbekMelikov <melikovasilbek04@gmail.com>
2024-05-06 16:34:40 +0500

    Bring track card information by useQuery and add them

--- client/src/components/layout.js
+++ client/src/components/layout.js
@@ -1,19 +1,46 @@
-import React from 'react';
-import { Header, Footer } from '../components';
-import styled from '@emotion/styled';
-import { widths, unit } from '../styles';
+import React from "react";
+import { Header, Footer, QueryResult } from "../components";
+import styled from "@emotion/styled";
+import { widths, unit } from "../styles";
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client";
+import TrackCard from "../containers/track-card";
 
 /**
  * Layout renders the full page content:
  * with header, Page container and footer
  */
-const Layout = ({ fullWidth, children, grid }) => {
+
+const TRACKS = gql`
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`;
+
+const Layout = ({ fullWidth, grid }) => {
+  const { loading, error, data } = useQuery(TRACKS);
+
   return (
     <>
       <Header />
-      <PageContainer fullWidth={fullWidth} grid={grid}>
-        {children}
-      </PageContainer>
+      <QueryResult loading={loading} error={error} data={data}>
+        <PageContainer fullWidth={fullWidth} grid={grid}>
+          {data?.tracksForHome?.map((track) => (
+            <TrackCard key={track.id} track={track} />
+          ))}
+        </PageContainer>
+      </QueryResult>
       <Footer />
     </>
   );
@@ -23,14 +50,14 @@ export default Layout;
 
 /** Layout styled components */
 const PageContainer = styled.div((props) => ({
-  display: 'flex',
-  justifyContent: props.grid ? 'center' : 'top',
-  flexDirection: props.grid ? 'row' : 'column',
-  flexWrap: 'wrap',
-  alignSelf: 'center',
+  display: "flex",
+  justifyContent: props.grid ? "center" : "top",
+  flexDirection: props.grid ? "row" : "column",
+  flexWrap: "wrap",
+  alignSelf: "center",
   flexGrow: 1,
   maxWidth: props.fullWidth ? undefined : `${widths.regularPageWidth}px`,
-  width: '100%',
+  width: "100%",
   padding: props.fullWidth ? 0 : unit * 2,
   paddingBottom: unit * 5,
 }));
